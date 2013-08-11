9ec51abc020bf3c651d588ecd57c4377e70a489e
Eric Haskins <ehask71@gmail.com>
2013-08-11 14:25:44 -0400

    Initial Load of cakePHP into the LeagueLaunchParent

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => '',
+		'database' => 'database_name',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+}

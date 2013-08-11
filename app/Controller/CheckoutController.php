ad7ed6086d8f82704fcf1916cbf025d8f9985704
Eric Haskins <ehask71@gmail.com>
2013-08-11 17:43:18 -0400

    Auth.net

--- app/Controller/CheckoutController.php
+++ app/Controller/CheckoutController.php
@@ -9,7 +9,7 @@ App::uses('AppController', 'Controller');
 class CheckoutController extends AppController {
 
     public $name = 'Checkout';
-    public $uses = array('Sites');
+    public $uses = array('Sites', 'OrderSaaS');
 
     public function beforeFilter() {
         parent::beforeFilter();
@@ -23,19 +23,29 @@ class CheckoutController extends AppController {
         $this->autoRender = false;
         print_r($this->request->data);
         if ($this->request->is('post') || $this->request->is('put')) {
-        if ($this->request->data['sid'] != '' && $this->request->data['oid'] != '' && $this->request->data['rtn'] != '') {
-            // Here we process the LL Checkouts
-            $site = $this->Sites->getSiteById($this->request->data['sid']);
-            
-            echo "<pre>";
-            print_r($site);
-        }
+            if ($this->request->data['sid'] != '' && $this->request->data['oid'] != '' && $this->request->data['rtn'] != '') {
+                echo "<pre>";
+                // Here we process the LL Checkouts
+                $site = $this->Sites->getSiteById($this->request->data['sid']);
+                if (count($site) > 0) {
+                    print_r($site);
+                    $order = $this->OrderSaaS->find('first', array(
+                        'conditions' => array(
+                            'OrderSaaS.id' => $this->request->data['oid']
+                        )
+                            ));
+                    if (count($order) > 0) {
+                        // We have an order
+                        print_r($order);
+                    }
+                }
+            }
         } else {
-          $this->redirect('/');
+            $this->redirect('/');
         }
     }
-    
-    public function testform(){
+
+    public function testform() {
         
     }
 

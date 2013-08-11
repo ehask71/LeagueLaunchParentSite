6d25057b5a5bf19d80f4f6118c6c724a4d048c50
Eric Haskins <ehask71@gmail.com>
2013-08-11 16:43:42 -0400

    Auth.net

---
+++ app/Controller/CheckoutController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * CakePHP CheckoutController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class CheckoutController extends AppController {
+    
+    public $name = 'Checkout';
+    public $uses = array('Settings','Sites');
+    
+    public function beforeFilter() {
+	parent::beforeFilter();
+    }
+    
+    public function index(){
+        
+    }
+    
+    public function ll(){
+        $this->autoRender = false;
+        //if ($this->request->is('post') || $this->request->is('put')) {
+            // Here we process the LL Checkouts
+            $site = $this->Sites->find('first',array(
+                'conditions' => array(
+                    'Sites.site_id' => $this->request->data['sid'] = 3
+                )));
+            
+            print_r($site);
+        //} else {
+          //  $this->redirect('/');
+        //}
+    }
+}
+

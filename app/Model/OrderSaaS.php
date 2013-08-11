424a100ac8eaabbd7242b497c773b01f20c014b2
Eric Haskins <ehask71@gmail.com>
2013-08-11 17:19:21 -0400

    Auth.net

---
+++ app/Model/OrderSaaS.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * CakePHP OrderSaaS
+ * @author Eric
+ */
+App::uses('AppModel', 'Model');
+
+class OrderSaaS extends AppModel {
+
+    public $primaryKey = 'id';
+    public $useDbConfig = 'SaaS';
+    public $useTable = 'orders';
+    public $hasMany = array(
+        'OrderItem' => array(
+            'className' => 'OrderItemSaaS',
+            'foreignKey' => 'order_id',
+            'dependent' => true,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => '',
+        )
+    );
+
+}
+

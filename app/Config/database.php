8e9f633012d52f392ed6e5147f3ca8e3a308da27
Eric Haskins <ehask71@gmail.com>
2013-08-11 16:02:11 -0400

    Initial Load of cakePHP into the LeagueLaunchParent

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,18 +1,18 @@
 <?php
+
 class DATABASE_CONFIG {
 
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => '',
-		'database' => 'database_name',
-		'prefix' => '',
-		'encoding' => 'utf8'
-	);
-        
-        public $SaaS = array(
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'user',
+        'password' => '',
+        'database' => 'database_name',
+        'prefix' => '',
+        'encoding' => 'utf8'
+    );
+    public $SaaS = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
         'host' => 'localhost',
@@ -24,4 +24,5 @@ class DATABASE_CONFIG {
         'prefix' => '',
         'encoding' => ''
     );
+
 }

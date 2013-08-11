625f0dcd651174c6e9d23029cab60db1a88ffe67
Eric Haskins <ehask71@gmail.com>
2013-08-11 17:12:44 -0400

    Auth.net

--- app/Model/Sites.php
+++ app/Model/Sites.php
@@ -19,7 +19,21 @@ class Sites extends AppModel {
     );
 
     public function getSiteById($id) {
+        $site = $this->find('first', array(
+            'conditions' => array(
+                'Sites.site_id' => $id
+                )));
         
+        if(count($site['Sites'])>0 && count($site['Settings'])>0){
+            $settings = array();
+            foreach ($site['Settings'] AS $set){
+                $settings[$set['name']] = $set['value'];
+            }
+            $site['Settings'] = $settings;
+            
+            return $site;
+        }
+        return false;
     }
 
 }

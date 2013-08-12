3ad739cd40fea2809997b311942b3edf0a4267fb
Eric Haskins <ehask71@gmail.com>
2013-08-11 22:24:46 -0400

    minYear set to 1950

--- app/Model/PlayersToSeasonsSaaS.php
+++ app/Model/PlayersToSeasonsSaaS.php
@@ -10,5 +10,22 @@ class PlayersToSeasonsSaaS extends AppModel {
     public $useDbConfig = 'SaaS';
     public $useTable = 'players_to_seasons';
     
+    public function updatePlayerHasPaid($player_id,$season_id,$site_id){
+        $player = $this->field('first', array(
+            'conditions' => array(
+                'PlayersToSeasonsSaaS.player_id' => $player_id,
+                'PlayersToSeasonsSaaS.season_id' => $season_id,
+                'PlayersToSeasonsSaaS.site_id' => $site_id
+            )
+        ));
+        if(isset($player['PlayersToSeasonsSaaS']['id'])){
+            $data = array();
+            $data['id'] = $player['PlayersToSeasonsSaaS']['id'];
+            $data['haspaid'] = 1;
+            $this->save($data);
+            return true;
+        }
+        return false;
+    }
 }
 

57eb8102d8380bc52c04c138a4a91acda31b92ab
Eric Haskins <ehask71@gmail.com>
2013-08-11 21:52:17 -0400

    minYear set to 1950

--- app/Controller/CheckoutController.php
+++ app/Controller/CheckoutController.php
@@ -20,46 +20,59 @@ class CheckoutController extends AppController {
         
     }
 
-    public function ll() {
+    public function ll($oid = false,$sid = false) {
 
         $this->autoRender = false;
-        if ($this->request->is('post') || $this->request->is('put')) {
-            if (isset($this->request->data['sid']) && isset($this->request->data['oid']) && isset($this->request->data['rtn'])) {
-                // Here we process the LL Checkouts
-                $site = $this->Sites->getSiteById($this->request->data['sid']);
-                if (count($site) > 0) {
-                    $this->Session->write('Sitedetails', $site);
-                    Configure::write('Settings.llcheckout.authorize_net_api_url', $site['Settings']['authorize_net_api_url']);
-                    Configure::write('Settings.llcheckout.authorize_net_login', $site['Settings']['authorize_net_login']);
-                    Configure::write('Settings.llcheckout.authorize_net_txnkey', $site['Settings']['authorize_net_txnkey']);
-                    $order = $this->OrderSaaS->find('first', array(
-                        'conditions' => array(
-                            'OrderSaaS.id' => $this->request->data['oid']
-                        )
-                            ));
-                    if (count($order) > 0) {
-                        // We have an order
-                        $this->Session->write('Orderdetails', $order);
-                        $this->render('/Elements/ll_checkout_step1');
-                    }
+        if ($sid && $oid) {
+            // Here we process the LL Checkouts
+            $site = $this->Sites->getSiteById($sid);
+            if (count($site) > 0) {
+                $this->Session->write('Sitedetails', $site);
+                $order = $this->OrderSaaS->find('first', array(
+                    'conditions' => array(
+                        'OrderSaaS.id' => $oid
+                    )
+                        ));
+                if (count($order) > 0) {
+                    // We have an order
+                    $this->set(compact('sid'));
+                    $this->set(compact('oid'));
+                    $this->Session->write('Orderdetails', $order);
+                    $this->render('/Elements/ll_checkout_step1');
                 }
-            } elseif ($this->request->data['Sites']['creditcard_number'] != '' && $this->request->data['Sites']['creditcard_month'] != '' && $this->request->data['Sites']['creditcard_year'] != '' && $this->request->data['Sites']['creditcard_code'] != '') {
-                echo "<pre>";
-                $site = $this->Session->read('Sitedetails');
-                $order = $this->Session->read('Orderdetails');
-                $txn = $this->AuthorizeNet->charge($order['OrderSaaS'],$this->request->data['Sites'],$site);
-                
-                print_r($txn);
-            } else {
-                print_r($this->request->data);
             }
         } else {
+            print_r($this->request->data);
             $this->redirect('/');
         }
     }
 
+    public function process() {
+        if ($this->request->is('post') || $this->request->is('put')) {
+            $this->autoRender = false;
+            if ($this->request->data['Sites']['creditcard_number'] != '' && $this->request->data['Sites']['creditcard_month'] != '' && $this->request->data['Sites']['creditcard_year'] != '' && $this->request->data['Sites']['creditcard_code'] != '') {
+                echo "<pre>";
+                $site = $this->Session->read('Sitedetails');
+                $order = $this->Session->read('Orderdetails');
+                try {
+                    $authorizeNet = $this->AuthorizeNet->charge($order['OrderSaaS'], $this->request->data['Sites'], $site);
+                } catch (Exception $e) {
+                    $this->Session->setFlash($e->getMessage());
+                    $this->redirect('/checkout/ll/'.$this->request->data['oid'].'-'.$this->request->data['sid']);
+                }
+                $order['OrderSaaS']['authorization'] = $authorizeNet[4];
+                $order['OrderSaaS']['transaction'] = $authorizeNet[6];
+
+                print_r($authorizeNet);
+            } else {
+                
+                $this->redirect('/checkout/ll/'.$this->request->data['oid'].'-'.$this->request->data['sid']);
+            }
+        }
+    }
+
     public function testform() {
-        
+        $this->redirect('/checkout/ll/52083e28-2020-4b59-929a-7926413c2bf7-3');
     }
 
 }

d903dfe716d06dc4a4873d16bbdee8a1a0ccef45
Eric Haskins <ehask71@gmail.com>
2013-11-05 15:20:01 -0500

    cart mods

--- app/Controller/EventController.php
+++ app/Controller/EventController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP EventController
  * @author Eric
@@ -8,13 +9,19 @@ App::uses('AppController', 'Controller');
 class EventController extends AppController {
 
     public $name = 'Event';
-    
+
     public function beforeFilter() {
-        parent::beforeFilter();
+	parent::beforeFilter();
     }
-    
-    public function index(){
-        
+
+    public function index($slug = null) {
+	if ($slug == null) {
+	    $this->Session->setFlash(__('We Were Unable To Locate That Event'), 'alert', array(
+		'plugin' => 'BoostCake',
+		'class' => 'alert-error'
+	    ));
+	    $this->redirect('/');
+	}
     }
-    
+
 }

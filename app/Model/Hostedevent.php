2b3c0244b743c8a46e40bab86303d6af95dfe0be
EricMain <EricMain@EricMain-PC.home>
2013-11-05 23:34:00 -0500

    Template adjustments

---
+++ app/Model/Hostedevent.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CakePHP Hostedevent
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class HostedeventModel extends Model {
+    
+    public $name = 'Hostedevent';
+    public $primaryKey = 'id';
+   
+    public function getHostedEventBySlug($slug){
+        $evt = $this->find('first',array(
+              'conditions' => array(
+                'Hostedevent.slug' => $slug,
+                'Hostedevent.active' => 1
+              )
+        ));
+        
+        return $evt;
+    }
+}

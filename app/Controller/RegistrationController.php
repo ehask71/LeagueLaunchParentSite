0f0dba0d5c49cb260c6b4805c674f4a9329c5906
Eric Haskins <ehask71@gmail.com>
2013-11-22 13:14:16 -0500

    Registration Controller

---
+++ app/Controller/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CakePHP RegistrationController
+ * @author Eric
+ */
+App::uses('AppController', 'Controller');
+
+class RegistrationController extends AppController {
+    
+    public $name = 'Registration';
+    
+    public $components = array('Session');
+
+
+    public function index(){
+        
+    }
+    
+}

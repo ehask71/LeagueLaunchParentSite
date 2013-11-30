8953fa295a5295148835235208752717d40515fb
Eric Haskins <ehask71@gmail.com>
2013-11-30 04:04:14 -0500

    New Registration theme - Clean

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP RegistrationController
  * @author Eric
@@ -6,29 +7,38 @@
 App::uses('AppController', 'Controller');
 
 class RegistrationController extends AppController {
-    
+
     public $name = 'Registration';
     public $uses = array('Sites');
     public $components = array('Session');
 
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->theme = (@$this->Session->read('Registration.theme') != '')?$this->Session->read('Registration.theme'):'regclean';
+        if ($this->params['action'] != 'index' || $this->params['action'] != 'notvalid') {
+            if (!$this->Session->check('Registration.site')) {
+                $this->Session->setFlash(__('Your Session Expired!'), 'alert', array(
+                    'plugin' => 'BoostCake',
+                    'class' => 'alert-error'
+                ));
+                $this->redirect('/registration/notvalid');
+            }
+        }
+        $this->theme = (@$this->Session->read('Registration.theme') != '') ? $this->Session->read('Registration.theme') : 'regclean';
     }
-    
-    public function index(){
+
+    public function index() {
         $this->autoRender = false;
-        if(isset($this->request->query['siteid']) && $this->request->query['siteid'] != ''){
-            $theme = (isset($this->request->query['theme']))?$this->request->query['theme']:'regclean';
-            
+        if (isset($this->request->query['siteid']) && $this->request->query['siteid'] != '') {
+            $theme = (isset($this->request->query['theme'])) ? $this->request->query['theme'] : 'regclean';
+
             $site = $this->Sites->find('first', array(
                 'conditions' => array(
                     'MD5(Sites.site_id) ' => $this->request->query['siteid'],
                     'Sites.isactive' => 'yes'
                 )
             ));
-            if(count($site) > 0){
-                $this->Session->write('Registration.theme',$theme);
+            if (count($site) > 0) {
+                $this->Session->write('Registration.theme', $theme);
                 $this->Session->write('Registration.site', $site);
                 $this->redirect('/registration/step1');
             } else {
@@ -38,40 +48,40 @@ class RegistrationController extends AppController {
             $this->redirect('/registration/notvalid');
         }
     }
-    
-    public function step1(){
+
+    public function step1() {
         $site = $this->Session->read('Registration.site');
         echo '<pre>';
         print_r($site);
         echo '</pre>';
     }
-    
+
     public function step2() {
-	
-    }
-    
-    public function step3() {
-	
-    }
-    
-    public function step4() {
-	
-    }
-    
-    public function step5() {
-	
-    }
-    
-    public function confirm() {
-	
-    }
-    
-    public function pay() {
-	
-    }
-    
-    public function notvalid(){
         
     }
-    
+
+    public function step3() {
+        
+    }
+
+    public function step4() {
+        
+    }
+
+    public function step5() {
+        
+    }
+
+    public function confirm() {
+        
+    }
+
+    public function pay() {
+        
+    }
+
+    public function notvalid() {
+        
+    }
+
 }

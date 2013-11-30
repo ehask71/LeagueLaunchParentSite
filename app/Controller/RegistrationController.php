dd7db9df9017d91b8d68d982524835c5be34dd34
Eric Haskins <ehask71@gmail.com>
2013-11-29 19:05:42 -0500

    New Offsite Registration

--- app/Controller/RegistrationController.php
+++ app/Controller/RegistrationController.php
@@ -8,13 +8,21 @@ App::uses('AppController', 'Controller');
 class RegistrationController extends AppController {
     
     public $name = 'Registration';
-    
+    public $uses = array('Sites');
     public $components = array('Session');
 
 
     public function index(){
+        $this->autoRender = false;
         if(isset($this->request->query['siteid']) && $this->request->query['siteid'] != ''){
-            
+            $site = $this->Sites->find('first', array(
+                'conditions' => array(
+                    'MD5(Sites.id) ' => $this->request->query['siteid']
+                )
+            ));
+            echo '<pre>';
+            print_r($site);
+            echo '</pre>';
         } else {
             $this->redirect('/registration/notvalid');
         }

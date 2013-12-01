0a525eef3ff53ada36395732f8afcfe8808fb801
Eric Haskins <ehask71@gmail.com>
2013-11-30 21:23:29 -0500

    nhh

--- app/Model/AccountSaaS.php
+++ app/Model/AccountSaaS.php
@@ -38,7 +38,7 @@ class AccountSaaS extends AppModel {
     );
     
     function __construct($id = false, $table = null, $ds = null) {
-	$this->hasAndBelongsToMany['RoleSaaS']['conditions'] = array('RolesUserSaaS.site_id' => Configure::read('Registration.site_id'));
+	$this->hasAndBelongsToMany['RoleSaaS']['conditions'] = array('RoleUserSaaS.site_id' => Configure::read('Registration.site_id'));
 	//$this->hasMany['Players']['conditions'] = array('Players.site_id' => Configure::read('Settings.site_id'));
 	parent::__construct($id, $table, $ds);
     }

3a50761d7f3584e382cad0145526aa8c9d312613
ehask71 <ehask71@gmail.com>
2013-12-01 22:53:25 -0500

    added Layout Helper

---
+++ app/Model/Datasource/MysqlEx.php
@@ -0,0 +1,20 @@
+<?php
+
+App::uses('Mysql', 'Model/Datasource/Database');
+
+/**
+ * Logs MySQL queries into debug file
+ * 
+ * @author  Vladimir Bilyov 2012-03-04 19:03:03
+ * @version 1.0
+ */
+class MysqlEx extends Mysql {
+
+    function logQuery($sql) {
+	parent::logQuery($sql);
+	if (Configure::read('Cake.logQuery')) {
+	    debug('sql[' . $this->_queriesCnt . ']:' . $sql);
+	}
+    }
+
+}

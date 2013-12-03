bd9b707ffa08325d61315ef023a9633126fb8712
Eric Haskins <ehask71@gmail.com>
2013-12-02 23:33:03 -0500

    SeasonSaaS model

--- app/Model/AccountSaaS.php
+++ app/Model/AccountSaaS.php
@@ -14,157 +14,157 @@ class AccountSaaS extends AppModel {
     //public $actsAs = array('Search.Searchable');
     public $primaryKey = 'id';
     public $hasAndBelongsToMany = array(
-	'RoleSaaS' => array(
-	    'className' => 'RoleSaaS',
-	    'joinTable' => 'roles_users',
-	    'foreignKey' => 'user_id',
-	    'assosciationForeignKey' => 'role_id',
-	    'unique' => 'keepExisting'
-	)
+        'RoleSaaS' => array(
+            'className' => 'RoleSaaS',
+            'joinTable' => 'roles_users',
+            'foreignKey' => 'user_id',
+            'assosciationForeignKey' => 'role_id',
+            'unique' => 'keepExisting'
+        )
     );
     public $hasMany = array(
-	'RoleUserSaaS' => array(
-	    'className' => 'RoleUserSaaS',
-	    'foreignKey' => 'user_id',
-	    'dependant' => true
-	),
+        'RoleUserSaaS' => array(
+            'className' => 'RoleUserSaaS',
+            'foreignKey' => 'user_id',
+            'dependant' => true
+        ),
     );
-    
     public $filterArgs = array(
         'firstname' => array('type' => 'like'),
         'lastname' => array('type' => 'like'),
         'email' => array('type' => 'like'),
-        //'filter' => array('type' => 'query', 'method' => 'orConditions'),
+            //'filter' => array('type' => 'query', 'method' => 'orConditions'),
     );
-    
+
     function __construct($id = false, $table = null, $ds = null) {
         App::import('Model', 'CakeSession');
-	$this->hasAndBelongsToMany['RoleSaaS']['conditions'] = array('RolesUser.site_id' => CakeSession::read('Registration.site.Sites.site_id'));
-	//$this->hasMany['Players']['conditions'] = array('Players.site_id' => Configure::read('Settings.site_id'));
-	parent::__construct($id, $table, $ds);
+        $this->hasAndBelongsToMany['RoleSaaS']['conditions'] = array('RolesUser.site_id' => CakeSession::read('Registration.site.Sites.site_id'));
+        //$this->hasMany['Players']['conditions'] = array('Players.site_id' => Configure::read('Settings.site_id'));
+        parent::__construct($id, $table, $ds);
     }
 
     public function beforeSave($options = array()) {
-	if (isset($this->data[$this->alias]['password'])) {
-	    $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-	}
-	return true;
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        }
+        return true;
     }
 
     public function beforeFind($query) {
 
 
-	return $query;
+        return $query;
     }
 
     public function getAssociatedUsers() {
-	
+        
     }
 
     public function accountValidate() {
-	$validate1 = array(
-	    'firstname' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please enter your First Name')
-	    ),
-	    'lastname' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please enter your Last Name')
-	    ),
+        $validate1 = array(
+            'firstname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter your First Name')
+            ),
+            'lastname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter your Last Name')
+            ),
             'address' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter your Address')
-	    ),
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your Address')
+            ),
             'city' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter your City')
-	    ),
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your City')
+            ),
             'state' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter your State')
-	    ),
-	    'zip' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter your Zip/Postal Code')
-	    ),
-	    'country' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Select Your Country'
-		)
-	    ),
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your State')
+            ),
+            'zip' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your Zip/Postal Code')
+            ),
+            'country' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Select Your Country'
+                )
+            ),
             'phone' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please enter Your Contact Number'
-		)
-	    ),
-	    'gender' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Select Your Gender'
-		)
-	    ),
-	    'email' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter Your Email'
-		),
-		'validEmailRule' => array(
-		    'rule' => 'email',
-		    'message' => 'Invalid email address'
-		),
-		'uniqueEmailRule' => array(
-		    'rule' => 'isUnique',
-		    'message' => 'Email already registered'
-		)
-	    ),
-	    'password' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Enter Your Password'
-		),
-		'passwordequal' => array(
-		    'rule' => 'checkpasswords',
-		    'message' => 'Passwords dont match'
-		)
-	    ),
-	    'confirm_password' => array(
-		'mustNotEmpty' => array(
-		    'rule' => 'notEmpty',
-		    'message' => 'Please Confirm Password'
-		)
-	    ),
-	    'agever' => array(
-		'notEmpty' => array(
-		    'rule' => array('comparison', '!=', 0),
-		    'required' => true,
-		    'message' => 'We require you to be over 13 to register without a Parent.'
-		)
-	    ),
-	    'agreeterms' => array(
-		'notEmpty' => array(
-		    'rule' => array('comparison', '!=', 0),
-		    'required' => true,
-		    'message' => 'Please Agree to the Terms if you want to proceed.'
-		)
-	    ),
-	);
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter Your Contact Number'
+                )
+            ),
+            'gender' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Select Your Gender'
+                )
+            ),
+            'email' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter Your Email'
+                ),
+                'validEmailRule' => array(
+                    'rule' => 'email',
+                    'message' => 'Invalid email address'
+                ),
+                'uniqueEmailRule' => array(
+                    'rule' => 'isUnique',
+                    'message' => 'Email already registered'
+                )
+            ),
+            'password' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter Your Password'
+                ),
+                'passwordequal' => array(
+                    'rule' => 'checkpasswords',
+                    'message' => 'Passwords dont match'
+                )
+            ),
+            'confirm_password' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Confirm Password'
+                )
+            ),
+            'agever' => array(
+                'notEmpty' => array(
+                    'rule' => array('comparison', '!=', 0),
+                    'required' => true,
+                    'message' => 'We require you to be over 13 to register without a Parent.'
+                )
+            ),
+            'agreeterms' => array(
+                'notEmpty' => array(
+                    'rule' => array('comparison', '!=', 0),
+                    'required' => true,
+                    'message' => 'Please Agree to the Terms if you want to proceed.'
+                )
+            ),
+        );
 
-	$this->validate = $validate1;
-	return $this->validates();
+        $this->validate = $validate1;
+        return $this->validates();
     }
 
     function checkpasswords() {
-	if (strcmp($this->data['Account']['password'], $this->data['Account']['confirm_password']) == 0) {
-	    return true;
-	}
-	return false;
+        if (strcmp($this->data['Account']['password'], $this->data['Account']['confirm_password']) == 0) {
+            return true;
+        }
+        return false;
     }
+
 }

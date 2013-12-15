5fb7577cd8108ea2ece5c006ac2532ca8ab02818
Eric Haskins <ehask71@gmail.com>
2013-12-15 11:08:02 -0500

    getProfileIds Auth.net

---
+++ app/Model/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * CakePHP Product
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class ProductModel extends Model {
+    
+    public $name = 'Product';
+    public $primaryKey = 'id';
+   
+    public function getProductsByCategoryId($id){
+        
+        $prod = $this->find('all',array(
+            'conditions' => array(
+                'Product.category_id' => $id,
+                'Product.visible' => 1
+            )
+        ));
+        
+        return $prod;
+    } 
+
+}

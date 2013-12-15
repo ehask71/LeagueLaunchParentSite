d205f2ec9f41167684e819d201c619825049d7ab
Eric Haskins <ehask71@gmail.com>
2013-12-15 17:10:52 -0500

    Fix declarations

---
+++ app/Model/EventRegistration.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * CakePHP EventRegistration
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class EventRegistrationModel extends Model {
+    
+    public $name = 'EventRegistration';
+    public $primaryKey = 'id';
+    public $validate = array(
+            'firstname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter your First Name')
+            ),
+            'lastname' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter your Last Name')
+            ),
+            'address' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your Address')
+            ),
+            'city' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your City')
+            ),
+            'state' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your State')
+            ),
+            'zip' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter your Zip/Postal Code')
+            ),
+            'country' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Select Your Country'
+                )
+            ),
+            'phone' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please enter Your Contact Number'
+                )
+            ),
+            'email' => array(
+                'mustNotEmpty' => array(
+                    'rule' => 'notEmpty',
+                    'message' => 'Please Enter Your Email'
+                ),
+                'validEmailRule' => array(
+                    'rule' => 'email',
+                    'message' => 'Invalid email address'
+                )
+            ),
+    );
+    
+}

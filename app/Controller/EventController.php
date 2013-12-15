e9d122808c02b2f58b4bede774b0e0e820abb004
Eric Haskins <ehask71@gmail.com>
2013-12-15 18:51:14 -0500

    Fix declarations

--- app/Controller/EventController.php
+++ app/Controller/EventController.php
@@ -82,7 +82,7 @@ class EventController extends AppController {
         if ($this->request->is('post')) {
             
         }
-        $this->theme = $this->Session->read('LLEvent.theme');
+        $this->theme = $this->Session->read('LLEvent.Hostedevent.theme');
 
         $this->set('event', $this->Session->read('Hostedevent'));
         $this->set('cart', $this->Session->read('Shop'));

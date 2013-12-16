70ffcdbb0f34843a97ea2a8abbee6d2d2cbe0a58
Eric Haskins <ehask71@gmail.com>
2013-12-16 03:52:44 -0500

    Fix declarations

---
+++ app/Model/OrderItem.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * CakePHP OrderItem
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class OrderItem extends Model {
+    
+    public $name = 'OrderItem';
+    public $primaryKey = 'id';
+   
+
+}

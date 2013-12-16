b12bcf68872f083c765e0e80510bd18175ffe599
Eric Haskins <ehask71@gmail.com>
2013-12-16 03:28:21 -0500

    Fix declarations

--- app/Model/Hostedevent.php
+++ app/Model/Hostedevent.php
@@ -21,4 +21,24 @@ class Hostedevent extends Model {
         
         return $evt;
     }
+    
+    public function transformDataOnePage($data){
+        $return = array();
+        $return['Order']['firstname'] = @$data['firstname'];
+        $return['Order']['lastname'] = @$data['lastname'];
+        $return['Order']['email'] = @$data['email'];
+        $return['Order']['phone'] = @$data['phone'];
+        $return['Order']['billing_address'] = @$data['address'];
+        $return['Order']['billing_city'] = @$data['city'];
+        $return['Order']['billing_state'] = @$data['state'];
+        $return['Order']['billing_zip'] = @$data['zip'];
+        $return['Order']['billing_country'] = @$data['country'];
+        $return['Order']['shipping_address'] = @$data['address'];
+        $return['Order']['shipping_city'] = @$data['city'];
+        $return['Order']['shipping_state'] = @$data['state'];
+        $return['Order']['shipping_zip'] = @$data['zip'];
+        $return['Order']['shipping_country'] = @$data['country'];
+        
+        return $return;
+    }
 }

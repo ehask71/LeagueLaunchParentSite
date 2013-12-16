0a69150a716fe09c662452ee77ec255d8860be4f
Eric Haskins <ehask71@gmail.com>
2013-12-16 03:49:58 -0500

    Fix declarations

---
+++ app/Model/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * CakePHP Order
+ * @author EricMain
+ */
+App::uses('Model', 'Model');
+
+class Order extends Model {
+    
+    public $name = 'Order';
+    public $primaryKey = 'id';
+   
+
+}

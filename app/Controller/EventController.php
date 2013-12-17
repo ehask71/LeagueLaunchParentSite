ef2c2e6b3da324d59eeb94bd32169b2d5e2de430
Eric Haskins <ehask71@gmail.com>
2013-12-17 00:16:37 -0500

    add complete function

--- app/Controller/EventController.php
+++ app/Controller/EventController.php
@@ -70,7 +70,7 @@ class EventController extends AppController {
     }
 
     public function confirm($slug = null) {
-        if (!$this->Session->check('LLEvent') && !$this->Session->check('Shop') && !$this->Session->check('Hostedevent')) {
+        if (!$this->Session->check('LLEvent') && !$this->Session->check('Shop') && !$this->Session->check('HostedEvent')) {
             $this->Session->setFlash(__('We Were Unable To Locate That Event or your Session Expired!'), 'alert', array(
                 'plugin' => 'BoostCake',
                 'class' => 'alert-error'

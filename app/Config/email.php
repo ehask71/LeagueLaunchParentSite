29031941ac2769ed77ef382c2647ed2153a84dea
Eric Haskins <ehask71@gmail.com>
2013-12-17 12:41:58 -0500

    Added Relation to Pdns_Record

---
+++ app/Config/email.php
@@ -0,0 +1,15 @@
+<?php
+
+class EmailConfig {
+
+    public $default = array(
+	'from' => array('do-not-reply@leaguelaunch.com' => 'League Launch'),
+	'host' => 'mail.leaguelaunch.com',
+	'port' => 25,
+	'username' => 'do-not-reply@leaguelaunch.com',
+	'password' => '87.~~?ZG}eI}',
+	'transport' => 'Smtp',
+	'client' => null,
+	'log' => false);
+
+}

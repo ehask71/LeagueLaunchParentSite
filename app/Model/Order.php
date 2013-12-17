b067ce8d73fa1167630cbb82dc445c122b8434d6
Eric Haskins <ehask71@gmail.com>
2013-12-16 19:56:14 -0500

    add complete function

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,9 +12,23 @@
 App::uses('Model', 'Model');
 
 class Order extends Model {
-    
+
     public $name = 'Order';
     public $primaryKey = 'id';
-   
+    public $hasMany = array(
+        'OrderItem' => array(
+            'className' => 'OrderItem',
+            'foreignKey' => 'order_id',
+            'dependent' => true,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => '',
+        )
+    );
 
 }
--- app/Model/OrderItem.php
+++ app/Model/OrderItem.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,9 +12,19 @@
 App::uses('Model', 'Model');
 
 class OrderItem extends Model {
-    
+
     public $name = 'OrderItem';
     public $primaryKey = 'id';
-   
+    public $belongsTo = array(
+        'Order' => array(
+            'className' => 'Order',
+            'foreignKey' => 'order_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'counterCache' => true,
+            'counterScope' => array(),
+        )
+    );
 
 }

2e7e5e4ef9d1bdc407d311aed0c0f12273b93fa5
ehaskins <ehaskins@erics-mbp.home>
2013-12-22 18:27:04 -0500

    New Model

--- app/Model/RegistrationSaaS.php
+++ app/Model/RegistrationSaaS.php
@@ -16,6 +16,42 @@ class RegistrationSaaS extends AppModel {
                 'message' => 'Please enter your last name.'
             ),
         ),
+        'email' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please enter your email.'
+            ),
+        ),
+        'billing_address' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please enter your billing address.'
+            ),
+        ),
+        'billing_city' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please enter your billing city.'
+            ),
+        ),
+        'billing_zip' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please enter your billing zip.'
+            ),
+        ),
+        'billing_country' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please select your billing country.'
+            ),
+        ),
+        'billing_state' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                'message' => 'Please enter your billing state.'
+            ),
+        ),
     );
 }
 

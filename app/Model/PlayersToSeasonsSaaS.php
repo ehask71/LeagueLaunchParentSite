922713d2ef0c832381e199e7f3516f95c79ddaff
ehaskins <ehaskins@erics-mbp.home>
2014-01-07 01:02:01 -0500

    Confirm validation

--- app/Model/PlayersToSeasonsSaaS.php
+++ app/Model/PlayersToSeasonsSaaS.php
@@ -58,5 +58,20 @@ class PlayersToSeasonsSaaS extends AppModel {
 	}
 	return $players;
     }
+    
+    public function addPlayer($season_id, $player, $div, $product_id, $opts = array()) {
+        $data['PlayersToSeasons'] = array();
+        $data['PlayersToSeasons']['season_id'] = (int) $season_id;
+        $data['PlayersToSeasons']['site_id'] = Configure::read('Settings.site_id');
+        $data['PlayersToSeasons']['player_id'] = (int) $player;
+        $data['PlayersToSeasons']['division_id'] = (int) $div;
+        $data['PlayersToSeasons']['product_id'] = (int) $product_id;
+        $data['PlayersToSeasons']['haspaid'] = (isset($opts['haspaid'])) ? $opts['haspaid'] : 0;
+        $data['PlayersToSeasons']['formcomplete'] = (isset($opts['haspaid'])) ? $opts['haspaid'] : 0;
+        $data['PlayersToSeasons']['verifydocs'] = (isset($opts['haspaid'])) ? $opts['haspaid'] : 0;
+
+        return $data;
+        
+    }
 
 }
